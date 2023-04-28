697f1eaa5b53ce1639751498aae8a26a7ea05436
Paul Dardeau <pauldardeau@gmail.com>
2023-04-27 19:43:04 -0500

    rename some class-level constants

--- Jukebox.pas
+++ Jukebox.pas
@@ -10,12 +10,12 @@ type
   public
     const DOUBLE_DASHES = "--";
 
-    // containers
-    const albumContainerSuffix = "albums";
-    const albumArtContainerSuffix = "album-art";
-    const metadataContainerSuffix = "music-metadata";
-    const playlistContainerSuffix = "playlists";
-    const SONG_CONTAINER_SUFFIX = "-artist-songs";
+    // container suffixes
+    const SFX_ALBUM_CONTAINER = "albums";
+    const SFX_ALBUM_ART_CONTAINER = "album-art";
+    const SFX_METADATA_CONTAINER = "music-metadata";
+    const SFX_PLAYLIST_CONTAINER = "playlists";
+    const SFX_SONG_CONTAINER = "-artist-songs";
 
     // directories
     const DIR_ALBUM_ART_IMPORT = "album-art-import";
@@ -149,7 +149,7 @@ begin
   for i := 0 to ArtistSongChars.Length-1 do begin
     const ch = ArtistSongChars[i];
     const ContainerName = aContainerPrefix +
-      String.Format("{0}{1}", ch, SONG_CONTAINER_SUFFIX);
+      String.Format("{0}{1}", ch, SFX_SONG_CONTAINER);
     if not StorageSys.CreateContainer(ContainerName) then begin
       writeLn("error: unable to create container '{0}'", ContainerName);
       exit false;
@@ -158,10 +158,10 @@ begin
 
   // create the other (non-song) containers
   var ContainerNames := new List<String>;
-  ContainerNames.Add(Jukebox.metadataContainerSuffix);
-  ContainerNames.Add(Jukebox.albumArtContainerSuffix);
-  ContainerNames.Add(Jukebox.albumContainerSuffix);
-  ContainerNames.Add(Jukebox.playlistContainerSuffix);
+  ContainerNames.Add(SFX_METADATA_CONTAINER);
+  ContainerNames.Add(SFX_ALBUM_ART_CONTAINER);
+  ContainerNames.Add(SFX_ALBUM_CONTAINER);
+  ContainerNames.Add(SFX_PLAYLIST_CONTAINER);
 
   for each ContainerName in ContainerNames do begin
     var CnrName := aContainerPrefix + ContainerName;
@@ -201,10 +201,10 @@ begin
   SongPlayDirPath := Utils.PathJoin(CurrentDir, DIR_SONG_PLAY);
   AlbumArtImportDirPath := Utils.PathJoin(CurrentDir, DIR_ALBUM_ART_IMPORT);
   MetadataDbFile := DEFAULT_DB_FILE_NAME;
-  MetadataContainer := ContainerPrefix + Jukebox.metadataContainerSuffix;
-  PlaylistContainer := ContainerPrefix + Jukebox.playlistContainerSuffix;
-  AlbumContainer := ContainerPrefix + Jukebox.albumContainerSuffix;
-  AlbumArtContainer := ContainerPrefix + Jukebox.albumArtContainerSuffix;
+  MetadataContainer := ContainerPrefix + SFX_METADATA_CONTAINER;
+  PlaylistContainer := ContainerPrefix + SFX_PLAYLIST_CONTAINER;
+  AlbumContainer := ContainerPrefix + SFX_ALBUM_CONTAINER;
+  AlbumArtContainer := ContainerPrefix + SFX_ALBUM_ART_CONTAINER;
   SongList := new List<SongMetadata>();
   NumberSongs := 0;
   SongIndex := -1;
@@ -443,7 +443,7 @@ begin
     ArtistLetter := Artist[0];
   end;
 
-  result := ContainerPrefix + ArtistLetter.ToLower + SONG_CONTAINER_SUFFIX;
+  result := ContainerPrefix + ArtistLetter.ToLower + SFX_SONG_CONTAINER;
 end;
 
 //*******************************************************************************
